a1c561329365042c9a56c3095102e0997a1aa88b
Gaurav Kukreti <Gaurav.kukreti@yahoo.com>
2022-09-22 08:32:14 +0800

    fish class added

---
+++ src/com/singtel/model/Fish.java
@@ -0,0 +1,20 @@
+package com.singtel.model;
+
+public class Fish {
+
+	private String fishname;
+	private String fishage;
+	public String getFishname() {
+		return fishname;
+	}
+	public void setFishname(String fishname) {
+		this.fishname = fishname;
+	}
+	public String getFishage() {
+		return fishage;
+	}
+	public void setFishage(String fishage) {
+		this.fishage = fishage;
+	}
+	
+}
