cacd21c1b41a59c837f3b740fe50a28c81e8d718
Gaurav Kukreti <Gaurav.kukreti@yahoo.com>
2022-09-22 13:30:11 +0800

    Dehradun java

---
+++ src/com/singtel/model/Dehradun.java
@@ -0,0 +1,16 @@
+package com.singtel.model;
+
+public class Dehradun {
+
+	private String cityname;
+
+	public String getCityname() {
+		return cityname;
+	}
+
+	public void setCityname(String cityname) {
+		this.cityname = cityname;
+	}
+	
+	
+}
